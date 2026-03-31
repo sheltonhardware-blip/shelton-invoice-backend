80db9536605482113a3fa8cc71144a8f5b25c519
sheltonhardware-blip <sheltonhardware@gmail.com>
2026-03-31 08:43:36 +0000

    fix syntax error mongo

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ mongoose.connect(process.env.MONGO_URL, {
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 }).then(() => {
   console.log('Connected to MongoDB');
 }).catch(err => {
